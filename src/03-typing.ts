f5929936691f69844f49d8068222b54ca265bdf9
lanzarot <luis-389@hotmail.com>
2022-09-04 16:20:41 -0500

    typedAnnotatios TS

---
+++ src/03-typing.ts
@@ -0,0 +1,18 @@
+(() => {
+
+  let myProductName: String = 'Product 1';
+
+  let myProductPrice: number = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase(); // puedo acceder fácilmente a los métodos String
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+
+  const myProductName2 = 'Product 1';
+})();
+
+
+
